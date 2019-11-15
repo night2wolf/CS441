6304a53a77c1e123f1947ffe138f97e8e1e928c5
Trevor Klinkenberg <tklinkenberg@scriptpro.com>
2019-11-15 07:56:37 -0600

    read & Convert
    Read in the file and convert to integer

--- pagerank/src/pagerank/core.clj
+++ pagerank/src/pagerank/core.clj
@@ -1,7 +1,14 @@
 (ns pagerank.core
    (:gen-class))
+
+   ;; This program displays Hello World
 (defn Pages []
-   (with-open [rdr (clojure.java.io/reader "resources/pages.txt")]
-   (reduce conj [] (line-seq rdr))))
+   (def string1 (slurp "C:/Users/tklinkenberg/Documents/CS441/Clojure_Project/CS441/pagerank/resources/pages_trim.txt"))
+   (def string2 (clojure.string/split-lines string1))
+   (def string3 (get string2 0))
+   (def string4 (clojure.string/split string3 #" "))
+   (def string5 (get string4 0))
+   (Integer. (re-find  #"\d+" string5 )))
+ 
 (Pages)
-;; "C:/Users/tklinkenberg/Documents/CS441/Clojure_Project/pagerank/resources/pages.txt"
+;; "C:/Users/tklinkenberg/Documents/CS441/Clojure_Project/CS441/pagerank/resources/pages.txt"
